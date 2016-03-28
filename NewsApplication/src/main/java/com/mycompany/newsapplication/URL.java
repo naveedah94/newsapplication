fb1d0ba35e4f6772e9e8282763f395dacf14408b
naveedah94 <naveed.ah94@gmail.com>
2016-03-28 14:00:07 +0500

    adding files

---
+++ NewsApplication/src/main/java/com/mycompany/newsapplication/URL.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.newsapplication;
+
+public class URL {
+    String urlEng = "http://www.thenews.com.pk/latest-stories";
+    public String getUrlEng() {
+    return urlEng;
+    }
+}
